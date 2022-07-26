2a620e4111517c7166384864cf71c2efb1b850f3
Kelvin Okechi <kelvinomenyi@gmail.com>
2022-07-26 08:18:52 -0700

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">kellyworld</a>
+      </header>
+      <main>list Products</main>
+    </div>
+  );
+}
+
+export default App;
